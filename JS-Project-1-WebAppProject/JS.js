d18ee54d3345e725fcee200083c1ace591f6d67c
HuangjieJin <jinhuangjie233@gmail.com>
2021-04-09 16:27:47 +1000

    Version 3

--- JS-Project-1-WebAppProject/JS.js
+++ JS-Project-1-WebAppProject/JS.js
@@ -9,7 +9,8 @@ function searchMovieData() {
   .then(json=> {
     let outerContainer = document.querySelector('#searchingresult');
     removeChildren(outerContainer)
-    json.Search.forEach(movie=>addMovieData(movie))
+    let MovieResult = removeDuplicates(json.Search)
+    MovieResult.forEach(movie=>addMovieData(movie))
     return console.log(json)
   })
 };
@@ -34,6 +35,51 @@ function removeChildren(element) {
     element.removeChild(child);
     child = element.lastElementChild;
   }
+}
+
+function removeDuplicates(arr) {
+
+  const result = [];
+  const duplicatesIndices = [];
+
+  // Loop through each item in the original array
+  arr.forEach((current, index) => {
+  
+      if (duplicatesIndices.includes(index)) return;
+  
+      result.push(current);
+  
+      // Loop through each other item on array after the current one
+      for (let comparisonIndex = index + 1; comparisonIndex < arr.length; comparisonIndex++) {
+      
+          const comparison = arr[comparisonIndex];
+          const currentKeys = Object.keys(current);
+          const comparisonKeys = Object.keys(comparison);
+          
+          // Check number of keys in objects
+          if (currentKeys.length !== comparisonKeys.length) continue;
+          
+          // Check key names
+          const currentKeysString = currentKeys.sort().join("").toLowerCase();
+          const comparisonKeysString = comparisonKeys.sort().join("").toLowerCase();
+          if (currentKeysString !== comparisonKeysString) continue;
+          
+          // Check values
+          let valuesEqual = true;
+          for (let i = 0; i < currentKeys.length; i++) {
+              const key = currentKeys[i];
+              if ( current[key] !== comparison[key] ) {
+                  valuesEqual = false;
+                  break;
+              }
+          }
+          if (valuesEqual) duplicatesIndices.push(comparisonIndex);
+          
+      } // end for loop
+
+  }); // end arr.forEach()
+
+  return result;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
